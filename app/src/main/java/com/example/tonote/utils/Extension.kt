fbe63a28f1545e23e921c8c11d040ed0fb6675a2
Kolitha Senevirathne <kolithasenevirathne@gmail.com>
2022-05-03 22:11:41 +0530

    small CRUD operation bug fixes

--- app/src/main/java/com/example/tonote/utils/Extension.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tonote.utils
-
-import android.content.Context.INPUT_METHOD_SERVICE
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.view.inputmethod.InputMethodManager.HIDE_NOT_ALWAYS
-
-fun View.hideKeyboard()= (context.getSystemService(INPUT_METHOD_SERVICE)as InputMethodManager)
-    .hideSoftInputFromWindow(windowToken,HIDE_NOT_ALWAYS)
