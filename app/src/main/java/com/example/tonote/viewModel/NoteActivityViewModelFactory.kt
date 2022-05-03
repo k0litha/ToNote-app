6bd7019147c4582f055800f254e36df51b2759db
Kolitha Senevirathne <kolithasenevirathne@gmail.com>
2022-05-03 22:18:38 +0530

    minor changes

---
+++ app/src/main/java/com/example/tonote/viewModel/NoteActivityViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.example.tonote.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.tonote.repository.NoteRepository
+
+class NoteActivityViewModelFactory(private val repository: NoteRepository):
+    ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
+        @Suppress("UNCHECKED_CAST")
+        return NoteActivityViewModel(repository) as T
+    }
+}
